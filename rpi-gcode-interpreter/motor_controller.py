c7bdca17abdcaee3e78e3215bc91c73868bb06e5
Tom <wyllietom@gmail.com>
2016-11-20 15:42:23 +0000

    Fixed bug in setting up pins in motor_controller script

--- rpi-gcode-interpreter/motor_controller.py
+++ rpi-gcode-interpreter/motor_controller.py
@@ -4,9 +4,8 @@
 ############################################################
 
 import sys
-"""
 import RPi.GPIO as GPIO
-"""
+
 
 class MotorController:
     def __init__(self):
@@ -29,29 +28,28 @@ class MotorController:
 
         # Tracks how many steps have been taken on each axis at any point
         self.current_steps = {'x': 0, 'y': 0, 'z': 0}
-        """
+
         # Configure pinout options
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
 
         # Setup motor control pins
-        for pins in self.control_pins:
-            for pin in pins:
-                GPIO.setup(pin, GPIO.OUT)
-        """
+        for pins in self.control_pins.values():
+                GPIO.setup(pins, GPIO.OUT)
+
     def axis_step(self, axis, direction):
 
         # Interacts with the pins to move the motor to the next step
 
         # Releases holding torque power from previously powered pin
-        # GPIO.output(self.control_pins[axis][self.current_steps[axis] % 4], False)
+        GPIO.output(self.control_pins[axis][self.current_steps[axis] % 4], False)
 
         # Increments counter that keeps track of which part of the (4-phase) rotation cycle we are in
         self.current_steps[axis] += direction
         # print(str(axis) + ": " + str(self.current_steps[axis]))
 
         # Power next pin in phase to drive motor
-        # GPIO.output(self.control_pins[axis][self.current_steps[axis] % 4], True)
+        GPIO.output(self.control_pins[axis][self.current_steps[axis] % 4], True)
 
 # Allows standalone running of the motor_controller script to move carts along axis by amount specified in args
 # Syntax is <axis> <direction> <steps> eg python motor_controller.py x -1 2400
